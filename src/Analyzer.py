Fixing Off-by-one error in Waveform_Creator.C

src/Analyzer.py
@@ -5,6 +5,7 @@ class Analyzer:
     N = 2128 # 3400 #Minimum number of time ticks present in SBND data arrays we're analyzing.
     PSDlength = 1065 # 1701 #the length of the PSD of the minwvfm
     Seconds_Per_Timetick = 0.5e-6 #0.5 microseconds per tick
+    Path_to_Validation = '/exp/dune/data/users/mking/ICEBERG_Noise_Ar_39/iceberg_noise/Sim_Validation_04192024/'
 
 
     def LoadWaveformFile(Waveform_File_Path='Waveforms.root',returnIndex=0):
@@ -48,9 +49,9 @@ class Analyzer:
         StartingChannelIndex = 0
 
         #If in-progress file exists, read from that.
-        my_file = Path('FFTs/'+key+'_FFT_In_Progress.npz')
+        my_file = Path(Analyzer.Path_to_Validation+'FFTs/'+key+'_FFT_In_Progress.npz')
         if my_file.is_file():
-            ExistingFFTEvent = np.load('FFTs/'+key+'_FFT_In_Progress.npz')
+            ExistingFFTEvent = np.load(Analyzer.Path_to_Validation+'FFTs/'+key+'_FFT_In_Progress.npz')
             FFTEvent = ExistingFFTEvent['FFT']
             StartingChannelIndex=ExistingFFTEvent['num']
 
@@ -60,9 +61,9 @@ class Analyzer:
         for iChannel in np.arange(StartingChannelIndex,np.shape(H)[0],1):
             FFTEvent[iChannel] = Analyzer.getFFT(H[iChannel])
             if iChannel%200 == 0:
-                np.savez('FFTs/'+key+'_FFT_In_Progress.npz',FFT = FFTEvent,num=iChannel)
+                np.savez(Analyzer.Path_to_Validation+'FFTs/'+key+'_FFT_In_Progress.npz',FFT = FFTEvent,num=iChannel)
         
-        np.savez('FFTs/'+key+'_FFT_In_Progress.npz',FFT = FFTEvent,num=Analyzer.numchannels)
+        np.savez(Analyzer.Path_to_Validation+'FFTs/'+key+'_FFT_In_Progress.npz',FFT = FFTEvent,num=Analyzer.numchannels)
 
         return FFTEvent
     
@@ -70,16 +71,16 @@ class Analyzer:
         import numpy as np
         from pathlib import Path
 
-        my_file = Path('FFTs/'+key+'_FFT_In_Progress.npz')
+        my_file = Path(Analyzer.Path_to_Validation+'FFTs/'+key+'_FFT_In_Progress.npz')
         if my_file.is_file():
-            ExistingFFTEvent = np.load('FFTs/'+key+'_FFT_In_Progress.npz')
+            ExistingFFTEvent = np.load(Analyzer.Path_to_Validation+'FFTs/'+key+'_FFT_In_Progress.npz')
             FFTEvent = ExistingFFTEvent['FFT']
             StartingChannelIndex=ExistingFFTEvent['num']
             if StartingChannelIndex == Analyzer.numchannels:
                 return FFTEvent
 
         FFT = np.abs(np.fft.rfft(H))
-        np.savez('FFTs/'+key+'_FFT_In_Progress.npz',FFT = FFT,num=Analyzer.numchannels)
+        np.savez(Analyzer.Path_to_Validation+'FFTs/'+key+'_FFT_In_Progress.npz',FFT = FFT,num=Analyzer.numchannels)
         
         return FFT
 

src/plotter.py
@@ -16,6 +16,26 @@ class plotter:
     SampleSpacing = 0.5e-6 #Seconds per time tick
     numfreqbins = 10
     
+    #convert_index_to_sorted
+    #Input a global channel number and get an array with the TPC, Plane, and Channel number
+    #Output: TPC, Plane, Channel
+    def convert_index_to_sorted(index):
+        if(index < 200):
+            return 0,0,index
+        elif(index < 400):
+            return 1,0,index-200
+        elif(index < 600):
+            return 0,1,index-400
+        elif(index < 800):
+            return 1,1,index-600
+        elif(index < 1040):
+            return 0,2,index-800
+        elif(index < 1280):
+            return 1,2,index-1040
+        else:
+            return -1,-1,-1
+
+
     #convert_to_sorted
     #MegaArray[GlobalChannel][OtherStuff...] is a numpy array with shape ()
     #SortedArray[tpc][plane][channel][OtherStuff...]
@@ -112,8 +132,8 @@ class plotter:
                 #For 2D plot
                 cmap = mpl.cm.get_cmap('viridis')
                 cmap.set_under('white')
-                ax2[tpc][plane].pcolormesh(freq,range(plotter.maxwires),np.log10(Sorted_PSD[tpc][plane]),cmap = cmap,shading='gouraud',vmin = 3.5, vmax = 4.5) #default -14,-5
-                fig2.colorbar(ax2[tpc][plane].pcolormesh(freq,range(plotter.maxwires),np.log10(Sorted_PSD[tpc][plane]),cmap = cmap,shading='gouraud',vmin = 3.5, vmax = 4.5)) #default -14,-5
+                ax2[tpc][plane].pcolormesh(freq,range(plotter.maxwires),Sorted_PSD[tpc][plane],cmap = cmap,shading='gouraud',vmin = -1, vmax = 1) #default -1.3,3.3
+                fig2.colorbar(ax2[tpc][plane].pcolormesh(freq,range(plotter.maxwires),Sorted_PSD[tpc][plane],cmap = cmap,shading='gouraud',vmin = -1, vmax = 1)) #default 1.3,3.3
                 if(plane!=2):
                     ax2[tpc][plane].set_ylim(0,200)
                 
@@ -121,7 +141,7 @@ class plotter:
                 for nChannel in range(len(Sorted_PSD[tpc][plane])):
                     ax[tpc][plane].scatter(freq,Sorted_PSD[tpc][plane][nChannel],s=0.05,color = colorvalues[nChannel])
                     
-                ax[tpc][plane].set_yscale("log")
+                ax[tpc][plane].set_yscale("linear") #log
                 ax[tpc][plane].set_xlabel("Freq [1e6 Hz]")
                 ax[tpc][plane].set_ylabel("Averaged FFT [1/Hz^0.5]")
                 ax[tpc][plane].set_title("Averaged Channel FFT for TPC: " + str(tpc) + " Wire Plane: " + planenames[plane])